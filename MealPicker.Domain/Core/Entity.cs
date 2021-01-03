b0b2987672e5a2d8350882b74ed92dd9b7eb918a
Stuart Bradley <stuart@juno.io>
2021-01-03 23:03:20 +0000

    Added Main Entities

---
+++ MealPicker.Domain/Core/Entity.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MealPicker.Domain.Core
+{
+    public abstract class Entity
+    {
+        public Guid Id { get; }
+
+        public Entity()
+        {
+        }
+        public Entity(Guid id)
+        {
+            Id = id;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var compareTo = obj as Entity;
+
+            if (ReferenceEquals(this, compareTo)) return true;
+            if (compareTo is null) return false;
+
+            return Id.Equals(compareTo.Id);
+        }
+
+        public static bool operator ==(Entity a, Entity b)
+        {
+            if (a is null && b is null)
+                return true;
+
+            if (a is null || b is null)
+                return false;
+
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(Entity a, Entity b)
+        {
+            return !(a == b);
+        }
+
+        public override int GetHashCode()
+        {
+            return (GetType().GetHashCode() * 907) + Id.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return GetType().Name + " [Id=" + Id + "]";
+        }
+    }
+}
---
+++ MealPicker.Domain/Shopper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MealPicker.Domain.Core;
+
+namespace MealPicker.Domain
+{
+    public class Shopper:Entity
+    {
+        private List<Meal> _meals;
+        public IReadOnlyList<Meal> Meals;
+    }
+}
